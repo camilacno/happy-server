8604b82fc0cec24fa10165eb550cbb59db5c5672
Camila Nepomuceno <camila.cno@gmail.com>
2020-10-13 15:12:18 -0300

    Images upload

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -32,6 +32,12 @@ export default {
     } = req.body;
 
     const orphanagesRepository = getRepository(Orphanage);
+
+    const requestImages = req.files as Express.Multer.File[];
+    const images = requestImages.map(image => {
+      return { path: image.filename }
+    })
+
     const orphanage = orphanagesRepository.create({
       name,
       latitude,
@@ -40,6 +46,7 @@ export default {
       instructions,
       opening_hours,
       open_on_weekends,
+      images,
     })
 
     await orphanagesRepository.save(orphanage);
