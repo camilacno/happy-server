4fa134a7a53595a759c7cb0257ba557429921aef
Camila Nepomuceno <camila.cno@gmail.com>
2020-10-14 20:14:53 -0300

    Create orphanage adjustments

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import path from 'path';
+import cors from 'cors';
 import 'express-async-errors';
 
 import './database/connection';
@@ -7,6 +8,8 @@ import routes from './routes';
 import errorHandler from './errors/handler';
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
